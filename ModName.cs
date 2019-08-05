7182c0109f302c81138e38bc1799b8c11d6e4083
gabrielhamel <gabriel.hamel@epitech.eu>
2019-08-05 19:49:08 +0200

    Initial commit

---
+++ ModName.cs
@@ -0,0 +1,20 @@
+using Terraria.ModLoader;
+
+namespace ModName
+{
+    public class ModName : Mod
+    {
+
+        public override void Load()
+        {
+
+        }
+
+        public override void Unload()
+        {
+
+        }
+
+    }
+
+}
